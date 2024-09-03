a765dddb450e7260873203dc5fcb11612e122911
Can Guven <cguven@wpi.edu>
2024-09-03 01:07:08 -0400

    tsting motor 2

--- test_motors.py
+++ test_motors.py
@@ -3,7 +3,7 @@ import time
 
 try:
     print("Testing motor forward.")
-    motor_control.motor_control(motor_control.motor4_pins, 'forward')
+    motor_control.motor_control(motor_control.motor2_pins, 'forward')
     time.sleep(2)
     print("Stopping motor.")
     motor_control.stop_all_motors()
