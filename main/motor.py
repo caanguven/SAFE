427827f1a23b87acfba64c48d3d9fa05f4900fcb
Can Guven <cguven@wpi.edu>
2024-09-15 19:57:18 -0400

    testing the new UI

--- main/motor.py
+++ main/motor.py
@@ -1,38 +1,42 @@
+# motor.py
+
 import RPi.GPIO as GPIO
 
 class Motor:
-    def __init__(self, in1_pin, in2_pin, spd_pin, pwm_frequency=1000):
+    def __init__(self, in1_pin, in2_pin, spd_pin):
+        # Set GPIO mode to BCM (Broadcom SOC channel numbering)
+        GPIO.setmode(GPIO.BCM)
+
         self.in1_pin = in1_pin
         self.in2_pin = in2_pin
         self.spd_pin = spd_pin
-        self.pwm_frequency = pwm_frequency
 
-        # Setup GPIO pins
+        # GPIO setup
         GPIO.setup(self.in1_pin, GPIO.OUT)
         GPIO.setup(self.in2_pin, GPIO.OUT)
         GPIO.setup(self.spd_pin, GPIO.OUT)
 
-        # Setup PWM
-        self.pwm = GPIO.PWM(self.spd_pin, self.pwm_frequency)
-        self.pwm.start(0)  # Start with 0% duty cycle (motor off)
+        # Initialize PWM on the speed pin
+        self.pwm = GPIO.PWM(self.spd_pin, 1000)  # PWM at 1kHz
+        self.pwm.start(0)
 
-    def set_speed(self, duty_cycle):
-        # Clamp duty_cycle to 0-100%
-        duty_cycle = max(0, min(100, duty_cycle))
-        self.pwm.ChangeDutyCycle(duty_cycle)
+    def set_speed(self, speed):
+        # Clamp the speed to 0-100%
+        speed = max(0, min(100, speed))
+        self.pwm.ChangeDutyCycle(speed)
 
     def forward(self):
-        GPIO.output(self.in1_pin, GPIO.LOW)
-        GPIO.output(self.in2_pin, GPIO.HIGH)
-
-    def backward(self):
         GPIO.output(self.in1_pin, GPIO.HIGH)
         GPIO.output(self.in2_pin, GPIO.LOW)
+
+    def backward(self):
+        GPIO.output(self.in1_pin, GPIO.LOW)
+        GPIO.output(self.in2_pin, GPIO.HIGH)
 
     def stop(self):
         GPIO.output(self.in1_pin, GPIO.LOW)
         GPIO.output(self.in2_pin, GPIO.LOW)
-        self.set_speed(0)
 
     def cleanup(self):
         self.pwm.stop()
+        GPIO.cleanup([self.in1_pin, self.in2_pin, self.spd_pin])
