9fa062fdfe960f7bcd22f3798aee231195908eb8
Can Guven <cguven@wpi.edu>
2024-11-25 00:39:29 -0500

    WP

--- main/data_collection/newSpike.py
+++ main/data_collection/newSpike.py
@@ -26,8 +26,8 @@ MOTOR3_ADC_CHANNEL = 2
 # ==========================
 # Constants for Control Signal Limiting
 # ==========================
-MAX_CONTROL_SIGNAL_DEAD_ZONE = 50  # Maximum control signal percentage in dead zone
-CONTROL_SIGNAL_INCREMENT_LIMIT = 10  # Max allowed change per control loop iteration
+MAX_CONTROL_SIGNAL_DEAD_ZONE = 45  # Maximum control signal percentage in dead zone
+CONTROL_SIGNAL_INCREMENT_LIMIT = 5  # Max allowed change per control loop iteration
 
 class SpikeFilter:
     def __init__(self, name):
