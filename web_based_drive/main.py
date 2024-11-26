621a062bb8622331010c86c24141412898af4b26
Can Guven <cguven@wpi.edu>
2024-11-25 23:32:21 -0500

    updates

--- web_based_drive/main.py
+++ web_based_drive/main.py
@@ -447,7 +447,7 @@ def shutdown():
 
 if __name__ == '__main__':
     try:
-        app.run(host='0.0.0.0', port=3000, threaded=True)
+        app.run(host='0.0.0.0', port=5000, threaded=True)
     except KeyboardInterrupt:
         logging.info("KeyboardInterrupt received. Shutting down.")
         motor_system.stop()
